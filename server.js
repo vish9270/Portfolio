02329e70c6a143e46c4192bc7335cdb9006599fe
vish9270 <vishveshu143@gmail.com>
2018-02-13 21:58:11 +0530

    'Prep_host_code'

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
+const PORT=process.env.PORT || 3000;
+app.use(function(req,res,next){
+  if(req.headers['x-forwaded-porto']=='http'){
+    next();
+  }
+  else{
+    res.redirect('http://'+ req.hostname + req.url);
+  }
+});
+
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Website is running on localhost:3000 ')
+app.listen(PORT, function () {
+  console.log('Website is running on localhost' +PORT);
 })
