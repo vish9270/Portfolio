34890562fa993cdc2978ef1fcaa51c1710b4fb72
vish9270 <vishveshu143@gmail.com>
2018-02-15 10:27:06 +0530

    'done_with_heroku_config'

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-var express = require('express');
-var app = express();
-const PORT=process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const PORT= process.env.PORT ||2000;
 app.use(function(req, res, next){
-  if(req.headers['x-forwaded-proto']=='http'){
+  if(req.headers['x-forwaded-proto'] =='http'){
     next();
-  }
-  else{
-    res.redirect('http://'+ req.hostname + req.url);
+  }else{
+    res.redirect('http://' +req.hostname +req.url);
   }
 });
-
-
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Website is running on localhost' +PORT);
-})
+  console.log('Website is running on localhost:'+ PORT);
+});
